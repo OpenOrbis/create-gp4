98453062266e5b231bacb6aed41573aa9cc4bf25
SpecterDev <cryptogenicdev@gmail.com>
2021-12-13 18:58:16 -0500

    Fix mod file references in workflows

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 cd ../cmd/create-gp4
 
 ## Windows
-GOOS=windows go build -o create-gp4.exe -modfile=go-linux.mod
+GOOS=windows go build -o create-gp4.exe
 mv ./create-gp4.exe ${OO_PS4_TOOLCHAIN}/bin/windows/create-gp4.exe
 
 ## Linux
-go build -o create-gp4 -modfile=go-linux.mod
+go build -o create-gp4
 mv ./create-gp4 ${OO_PS4_TOOLCHAIN}/bin/linux/create-gp4
 
 ## MacOS
-GOOS=darwin GOARCH=amd64 go build -o create-gp4osx -modfile=go-linux.mod
+GOOS=darwin GOARCH=amd64 go build -o create-gp4osx
 mv ./create-gp4osx ${OO_PS4_TOOLCHAIN}/bin/macos/create-gp4
